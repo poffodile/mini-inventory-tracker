eb85d82e145b93c8a3e6ea4079c13dfc202d6af5
Precious <lamberville73@gmail.com>
2025-08-16 09:08:02 +0100

    implemented navigation and added pages for dashboard, inventory, receive, pick, and import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.Dashboard),
+  },
+  {
+    path: 'inventory',
+    loadComponent: () => import('./pages/inventory/inventory').then((m) => m.Inventory),
+  },
+  {
+    path: 'receive',
+    loadComponent: () => import('./pages/receive/receive').then((m) => m.Receive),
+  },
+  {
+    path: 'pick',
+    loadComponent: () => import('./pages/pick/pick').then((m) => m.Pick),
+  },
+  {
+    path: 'import',
+    loadComponent: () => import('./pages/import/import').then((m) => m.Import),
+  },
+];
---
+++ src/app/pages/receive/receive.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receive',
+  imports: [],
+  templateUrl: './receive.html',
+  styleUrl: './receive.css'
+})
+export class Receive {
+
+}
