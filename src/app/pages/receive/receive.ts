aeede3ae465d95f9e1b15b91c0c169ece80de89c
Precious <lamberville73@gmail.com>
2025-08-19 01:17:53 +0100

    Created goods receipt form with input fields for product ID, quantity, and location

--- src/app/pages/receive/receive.ts
+++ src/app/pages/receive/receive.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-receive',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './receive.html',
-  styleUrl: './receive.css'
+  styleUrls: ['./receive.css'],
 })
 export class Receive {
+  productId: string = '';
+  quantity: number = 1;
+  locationId: string = '';
 
+  submitForm() {
+    // Handles  form submission
+    console.log('Form submitted and received :', {
+      productId: this.productId,
+      quantity: this.quantity,
+      locationId: this.locationId,
+    });
+    alert('Form submitted and recieved successfully!');
+  }
 }
